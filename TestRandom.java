4c25d8e0c6658014cf214c85deb34f431a542172
shadin shalaata <shadinshalaata@shadins-MacBook-Pro.local>
2024-11-19 11:28:14 +0200

    TestRandom Done

--- TestRandom.java
+++ TestRandom.java
@@ -2,5 +2,44 @@
 public  class  TestRandom {
 	public static void main(String[]  args) {
 	    // Replace this comment with your code
-	}
+		int N=Integer.parseInt(args[0]);
+		int countabove=0;
+		int countbelow=0;
+		while(N>0)
+		{
+			double a=(double)(Math.random()*1);
+			
+			if(a>0.5)
+			{
+				countabove++;
+			}
+			else
+			{
+				countbelow++;
+			}
+			N--;
+			
+	    }
+		if(countabove==0||countbelow==0)
+		{
+			System.out.println("> 0.5: "+countabove+" times");
+			System.out.println("<= 0.5: "+countbelow+" times");
+		}
+		else {
+			System.out.println("> 0.5: "+countabove+" times");
+			System.out.println("<= 0.5: "+countbelow+" times");
+			if(countabove>countbelow)
+			{
+				System.out.println("Ratio: "+(((double)countbelow)/countabove)+" times");
+			}
+			else
+			{
+				System.out.println("Ratio: "+(((double)countabove)/countbelow)+" times");
+			}
+
+
+		}
+
+		}
+		
 }
